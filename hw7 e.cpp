0cca53b44e581391ad40a1ecfb382309f4dc7aea
Sean <sean8388608@gmail.com>
2019-11-21 14:53:27 +0800

    edit hw7

--- hw7 e.cpp
+++ hw7 e.cpp
@@ -10,23 +10,26 @@ int main(){
     double fit,fitr,fitl,tmp = 100;
     srand(114);
     x0 = rand()/(float)RAND_MAX*(domainu-domaind)+domaind;
+    x = x0;
     step = powf(2.0, (int)log2(domainu) - 11);
+    stepi = step;
     fit = fabs(x0);
     fitr = fabs(x0 + step);
     fitl = fabs(x0 - step);
     n += 3;
-    while(x + step != x){
-
-        if(fitr < fitl){
+    cout << setprecision(20) << n << ' ' << x << ' ' << fit << ' ' << step << endl;
+    while(x + step != x){                       
+        if(fitr < fitl){                                       //左右看
             x = x0 + step;
         }else{
             x = x0 - step;
-        }
-        while(a == 0){
+        }                                   
+        while(a == 0){                                         //加速
             step *= 2.0;
             x += step;
             fit = fabs(x);
-            n += 1;
+            n ++;
+            
             if(fit < tmp){
                 tmp = fit;
                 best = x;
@@ -34,17 +37,44 @@ int main(){
                 a = 1;
                 step /= 2;
             }
-        //cout << setprecision(20) << x << ' ' << fit << ' ' << step << endl;
+            cout << setprecision(20) << n << ' ' << x << ' ' << fit << ' ' << step << endl;
 
         }
-        while(step != stepi){
+        step /= 2;
+        while(step != stepi){                                  //減速
+            
             fitr = fabs(x + step);
             fitl = fabs(x - step);
             n ++;
             cout << setprecision(20) << n << ' ' << x + step << ' ' << fitr << ' ' << step << endl;
             n ++;
             cout << setprecision(20) << n << ' ' << x - step << ' ' << fitl << ' ' << step << endl;
+            if(fitr < fitl){
+                if(fitr < fabs(best)){
+                    break;
+                    best = x + step;
+                    step *= 2;
+                }else{
+                    step /= 2;
+                    best = x + step;
+                }
+                x = best;
+            }else if(fitr > fitl){
+                if(fitl < fabs(best)){
+                    break;
+                    best = x - step;
+                    step *= 2;
+                }else{
+                    step /= 2;
+                    best = x - step;
+                }
+                x = best;
+            }else{
+                step /= 2;
+                x = best;
+            }
         }
-
+        step /= 2;
     }
+    system("pause");
 }
