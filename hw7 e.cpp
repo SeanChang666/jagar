930cde3bc240ea419bd7e621562460d7839842fb
Sean <sean8388608@gmail.com>
2019-11-26 23:48:57 +0800

    add hw7

--- hw7 e.cpp
+++ hw7 e.cpp
@@ -6,75 +6,130 @@ using namespace std;
 int main(){
     float step,stepi;
     float x0,x,best,domainu=100,domaind=-100;
-    int a = 0,n = 0,n0 = 0;
-    double fit,fitr,fitl,tmp = 100;
+    int n = 0,n0 = 0,skip = 0,dir;
+    double fit,fitr,fitl,tmp = 100,fitb;
     srand(114);
-    x0 = rand()/(float)RAND_MAX*(domainu-domaind)+domaind;
-    x = x0;
+    x = rand()/(float)RAND_MAX*(domainu-domaind)+domaind;
     step = powf(2.0, (int)log2(domainu) - 11);
     stepi = step;
-    fit = fabs(x0);
-    fitr = fabs(x0 + step);
-    fitl = fabs(x0 - step);
-    n += 3;
-    cout << setprecision(20) << n << ' ' << x << ' ' << fit << ' ' << step << endl;
-    while(x + step != x){                       
-        if(fitr < fitl){                                       //左右看
-            x = x0 + step;
-        }else{
-            x = x0 - step;
-        }                                   
-        while(a == 0){                                         //加速
-            step *= 2.0;
-            x += step;
-            fit = fabs(x);
-            n ++;
-            
-            if(fit < tmp){
-                tmp = fit;
-                best = x;
-            }else{
-                a = 1;
-                step /= 2;
+    fit = fabs(x);
+    n++;
+    cout << setprecision(50) << n << ' ' << x << ' ' << fit << ' ' << step << endl;
+    while(x + step != x){
+        //cout << "rlrlrlrlrlrl"<< endl;           
+        fitr = fabs(x + step);
+        fitl = fabs(x - step);
+        n++;
+        cout << setprecision(50) << n << ' ' << x+step << ' ' << fitr << ' ' << step << endl;
+        n++;
+        cout << setprecision(50) << n << ' ' << x-step << ' ' << fitl << ' ' << step <<endl;            
+        if(fitr < fitl){                     //左右看
+            if(fitr < tmp){                  //找到右邊較好的解
+                skip = 1;
+                tmp = fitr;
+                x += step;
+                dir = 1;
+            }                                     
+        }else if(fitr > fitl){
+            if(fitl < tmp){                  //找到左邊較好的解
+                skip = 1;
+                tmp = fitl;
+                x -= step;
+                dir = 0;
             }
-            cout << setprecision(20) << n << ' ' << x << ' ' << fit << ' ' << step << endl;
-
         }
+        //cout << "pppppp"<< endl;                                   
+        while(skip == 1){                                         //加速
+            step *= 2;
+            if(dir == 1){
+                if(x + step > 100){
+                    step /= 2;
+                    fit = fabs(x+step);
+                    n++;
+                    cout << setprecision(50) << n << ' ' << x + step << ' ' << fit << ' ' << step << endl;
+                    skip = 0;
+                    break;
+                }
+                fit = fabs(x+step);
+                n++;
+                cout << setprecision(50) << n << ' ' << x+step << ' ' << fit << ' ' << step << endl;
+                fitb = fabs(x);
+                if(fit < fitb){
+                    tmp = fit;
+                    x += step;
+                    
+                }else{
+                    step /= 2;
+                    fit = fabs(x+step);
+                    skip = 0;
+                    n++;
+                    cout << setprecision(50) << n << ' ' << x+step << ' ' << fit << ' ' << step << endl;
+                }
+            }else{
+                if(x - step < -100){
+                    step /= 2;
+                    fit = fabs(x-step);
+                    n++;
+                    cout << setprecision(50) << n << ' ' << x - step << ' ' << fit << ' ' << step << endl;
+                    skip = 0;
+                    break;
+                }
+                fit = fabs(x-step);
+                n++;
+                cout << setprecision(50) << n << ' ' << x-step << ' ' << fit << ' ' << step <<endl;
+                fitb = fabs(x);
+                if(fit < fitb){
+                    tmp = fit;
+                    x -= step;
+                    
+                }else{
+                    step /= 2;
+                    fit = fabs(x-step);
+                    skip = 0;
+                    n++;
+                    cout << setprecision(50) << n << ' ' << x-step << ' ' << fit << ' ' << step <<endl;
+                }
+            }
+            
+            
+            
+        }
+        //cout << "mmmmmmmm"<< endl;
         step /= 2;
-        while(step != stepi){                                  //減速
+        while(step != stepi/2){                                      //step回到初始//減速
             
             fitr = fabs(x + step);
             fitl = fabs(x - step);
             n ++;
-            cout << setprecision(20) << n << ' ' << x + step << ' ' << fitr << ' ' << step << endl;
+            cout << setprecision(50) << n << ' ' << x + step << ' ' << fitr << ' ' << step << endl;
             n ++;
-            cout << setprecision(20) << n << ' ' << x - step << ' ' << fitl << ' ' << step << endl;
+            cout << setprecision(50) << n << ' ' << x - step << ' ' << fitl << ' ' << step << endl;
             if(fitr < fitl){
-                if(fitr < fabs(best)){
-                    break;
-                    best = x + step;
-                    step *= 2;
-                }else{
-                    step /= 2;
-                    best = x + step;
-                }
-                x = best;
-            }else if(fitr > fitl){
-                if(fitl < fabs(best)){
-                    break;
-                    best = x - step;
-                    step *= 2;
-                }else{
-                    step /= 2;
-                    best = x - step;
-                }
-                x = best;
+                if(fitr < tmp){                  
+                    tmp = fitr;
+                    //cout << setprecision(50) << n << ' ' << x + step << ' ' << fit << ' ' << step << endl;
+                    x += step;
+                }   
+                step /= 2;                    
             }else{
+                if(fitl < tmp){                  
+                    tmp = fitl;
+                    //cout << setprecision(50) << n << ' ' << x - step << ' ' << fit << ' ' << step << endl;
+                    x -= step;if(x + step > 100){
+                    step /= 2;
+                    fit = fabs(x+step);
+                    n++;
+                    cout << setprecision(50) << n << ' ' << x << ' ' << fit << ' ' << step << endl;
+                    break;
+                    }
+                }
                 step /= 2;
-                x = best;
             }
+            
         }
-        step /= 2;
+        //step /= 2;
+        stepi /= 2;
+        step = stepi;
     }
     system("pause");
 }
